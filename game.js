bdd90dd8ee3fed0cbf6c4d10f98ff77ac6bb97da
Rabee Hanna <Rabee.hanna@mail.utoronto.ca>
2015-06-01 14:46:47 -0400

    Added a new game type, with working score system. Currently still experimental.

--- game.js
+++ game.js
@@ -1,15 +1,18 @@
 var COLUMNS = 4;
-var	ROWS = 8;
+var	ROWS = 9;
 var	INTERVAL = 350;
 
 var gameInterval;
+var score = 0;
 
 function createTable(columns, rows) {
 	var html = '<table>';
 	for (var i = 0; i < rows; i ++) {
 		html += '<tr class="row' + i + '">';
 		for (var j = 0; j < columns; j ++) {
-			if (i == rows - 1) {
+			if (i == 0) {
+				html += '<td class="column' + j + ' top"></td>';
+			} else if (i == rows - 1) {
 				html += '<td class="column' + j + ' button"></td>';
 			} else {
 				html += '<td class="column' + j + '"></td>';
@@ -27,7 +30,7 @@ function clearTable() {
 }
 
 function getTile(column, row) {
-	return $('.row' + (row) + ' .column' + column);
+	return $('.row' + row + ' .column' + column);
 }
 
 function getCoords(tile) {
@@ -36,9 +39,14 @@ function getCoords(tile) {
 	return [column, row];
 }
 
-function nextTile(tile) {
+function upTile(tile) {
 	var coords = getCoords(tile);
-	return getTile(coords[0], coords[1] - 1);
+	return getTile(coords[0], (parseInt(coords[1]) - 1));
+}
+
+function downTile(tile) {
+	var coords = getCoords(tile);
+	return getTile(coords[0], (parseInt(coords[1]) + 1));
 }
 
 function toggle(tile, position) {
@@ -57,29 +65,77 @@ function off(tile) {
 	toggle(tile, 'off');
 }
 
-function buttonHandler() {
+function buttonHandler0() {
 	off($('.button'));
 	on($(this));
+}
+
+function buttonHandler1() {
+	if ($(this).attr("data-on") == 'on') {
+		// gain points
+		score += 10;
+	} else {
+		// lose points
+		score -= 30;
+	}
 }
 
 function moveUp() {
 	$('td[data-on=on]').each(function() {
 		off($(this));
-		on(nextTile($(this)));
+		on(upTile($(this)));
 	});
 }
 
-function start() {
+function game0Step() {
+	score += 10;
+	moveUp();
+	$("#scoreVal").html(score);
+}
+
+function game1Step() {
+	moveDown();
+	$("#scoreVal").html(score);
+}
+
+function moveDown() {
+	$('td[data-on=on]').each(function() {
+		on(downTile($(this)));
+		off($(this));
+	});
+}
+
+function incomingTile(columns) {
+	var c = Math.floor(Math.random() * columns);
+	off($('.top'));
+	on(getTile(c, 0));
+}
+
+var u;
+
+function start(type) {
 	createTable(COLUMNS, ROWS);
-	$('.button').click(buttonHandler);
 	$(window).unload(function() {
 		clearInterval(gameInterval);
 	});
-	gameInterval = setInterval(moveUp, INTERVAL);
+
+	if (type == 0) {
+		$('.button').click(buttonHandler0);
+		gameInterval = setInterval(game0Step, INTERVAL);
+	} else if (type == 1) {
+		// For now, explicityly call incomeingTile(4) to simulate incoming tile
+		$('.button').click(buttonHandler1);
+		gameInterval = setInterval(game1Step, INTERVAL);
+
+		u = setInterval(function(){incomingTile(4)}, 250);
+	}
 }
 
 function end() {
 	clearInterval(gameInterval);
-	$('.button').off('click', buttonHandler);
+	clearInterval(u);
+	$('.button').off('click');
 	clearTable();
+	$("#scoreVal").html('-');
+	score = 0;
 }
--- page.js
+++ page.js
@@ -4,14 +4,16 @@ function displaySection(section) {
 }
 
 $(function() {
-	$("#playButton").click(function () {
+	$("#playButton0").click(function () {
 		displaySection("game");
-		start();
-	})
-
+		start(0);
+	});
+	$("#playButton1").click(function () {
+		displaySection("game");
+		start(1);
+	});
 	$("#backButton").click(function () {
 		end();
 		displaySection("main");
-	})
-
+	});
 });
